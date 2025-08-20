ef011a0a27b5e5a2e7d203e3cef248b77bde86b0
Wael Rahhal <65128166+Waelr1985@users.noreply.github.com>
2025-08-20 17:24:46 +0300

    Add run_etl helper and restructure tests

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from pyspark.sql import SparkSession
+
+
+@pytest.fixture(scope="session")
+def spark():
+    spark = SparkSession.builder.master("local[*]").appName("tests").getOrCreate()
+    yield spark
+    spark.stop()
